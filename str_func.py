1e22ae1b3b020b3aed0544cdfe307f313f80c72a
Umys <kipiani182@gmail.com>
2023-09-20 00:03:59 +0400

    Added uppercase_first_letters function and docstring to uppercase_str function

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
-def uppercase_str(input_str):
-    return input_str.upper()
+def uppercase_first_letters(input_str):
+    """
+    Converts the first letter of each word in input string to uppercase.
+    :param input_str: str value to convert the first letter of each word to uppercase
+    :return: str value of input_str with the first letter of each word in uppercase
+    """
+    return ' '.join(word.capitalize() for word in input_str.split())
