a22199e19ac03711172e06fde2acc82cb86859d4
Edisheri <145288760+Edisheri@users.noreply.github.com>
2023-09-20 00:03:17 +0400

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def uppercase_str(input_str):
+    """
+    Converts the input string to uppercase.
+    :param input_str: str value to convert to uppercase
+    :return: str value of input_str in uppercase
+    """
     return input_str.upper()
